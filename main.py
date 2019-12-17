b043644ce5ad9217d1fadd92d1ee8ed11f655413
ShibaeO <antoine@quetel.pro>
2019-12-17 22:56:48 +0100

    Update :         - mise en fonction des element pour           le changement de compte Add    :         - Account.json (Seront stocker info sur les compte)         - Fonction qui retourne le compte connecté depuis le registre

--- main.py
+++ main.py
@@ -3,30 +3,47 @@ from PyVDF import PyVDF
 
 # A bout but de changer deux valeurs dans le registre ("ActiveProcess") & ("AutoLoginUser")
 
-regActiveUser = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam\\ActiveProcess", access=winreg.KEY_ALL_ACCESS)
-
-dataActiveUser1 = 862238484
-dataActiveUser2 = 184418774
-
-winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, dataActiveUser1)
-decValue, idex = winreg.QueryValueEx(regActiveUser, "ActiveUser")
-print(decValue)
-
+dataActiveUser1 = 862238484 #sixthaccountcsgo
+dataActiveUser2 = 184418774 #antoineditlolotte
 dataStr1 = "antoineditlolotte"
 dataStr2 = "sixthaccountcsgo"
+curentLoggedAccount = "antoineditlolotte"
 
-regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
-winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, dataStr2)
-account, index = winreg.QueryValueEx(regLastLogin, "AutoLoginUser")
-print("le compte acctuelement connecter est : " + account + " sa value decimal est : " + str(decValue))
+def regModAutologin (AccountName):
+    regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
+    winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, AccountName)
+    account, index = winreg.QueryValueEx(regLastLogin, "AutoLoginUser")
+    print("le compte acctuelement connecter est : " + account)
+
+
+def regModActiveProcess (accountValue):
+    regActiveUser = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam\\ActiveProcess", access=winreg.KEY_ALL_ACCESS)
+    winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, dataActiveUser2)
+    decValue, idex = winreg.QueryValueEx(regActiveUser, "ActiveUser")
+    print(decValue)
+
+
+def regQuerryCurrenLogged():
+        regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
+        account, index = winreg.QueryValueEx(regLastLogin, "AutoLoginUser")
+        return account
+
+regModAutologin(dataStr1)
+
+if regQuerryCurrenLogged() == curentLoggedAccount:
+    print("compte deja conecter")
+    print(regQuerryCurrenLogged())
+else:
+    print("compte switché")
+    print(regQuerryCurrenLogged())
 
 # A pour but de changer le status dans le fichier ("loginusers.vdf") ce qui change le dernier utlisateur connectés
 
-vdf = PyVDF()
-vdf.load('loginusers.vdf')
-vdf.edit("users.76561198822504212.MostRecent", "0")
-vdf.write_file('loginusers.vdf')
-User = vdf["users"]["76561198822504212"]
-State = vdf["users"]["76561198822504212"]["MostRecent"]
-print(User)
-print(State)
+#vdf = PyVDF()
+#vdf.load('loginusers.vdf')
+#vdf.edit("users.76561198822504212.MostRecent", "0")
+#vdf.write_file('loginusers.vdf')
+#User = vdf["users"]["76561198822504212"]
+#State = vdf["users"]["76561198822504212"]["MostRecent"]
+#print(User)
+#print(State)
