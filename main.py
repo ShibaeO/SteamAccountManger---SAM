dfacfc9df58f7717ee3f9abdb562a6a963c5b6df
ShibaeO <antoine@quetel.pro>
2019-12-17 22:11:24 +0100

    Fix :      - Unpack des tuple du retour QueryValueEx

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ dataActiveUser1 = 862238484
 dataActiveUser2 = 184418774
 
 winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, dataActiveUser1)
-print(winreg.QueryValueEx(regActiveUser, "ActiveUser"))
+decValue, idex = winreg.QueryValueEx(regActiveUser, "ActiveUser")
+print(decValue)
 
 dataStr1 = "antoineditlolotte"
 dataStr2 = "sixthaccountcsgo"
 
 regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
 winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, dataStr2)
-print(winreg.QueryValueEx(regLastLogin, "AutoLoginUser"))
+account, index = winreg.QueryValueEx(regLastLogin, "AutoLoginUser")
+print("le compte acctuelement connecter est : " + account + " sa value decimal est : " + str(decValue))
 
 # A pour but de changer le status dans le fichier ("loginusers.vdf") ce qui change le dernier utlisateur connectés
 
-
 vdf = PyVDF()
 vdf.load('loginusers.vdf')
-
 vdf.edit("users.76561198822504212.MostRecent", "0")
 vdf.write_file('loginusers.vdf')
 User = vdf["users"]["76561198822504212"]
