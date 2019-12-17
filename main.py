75f88681262df11ec5127cc68b3b3bd431520d76
ShibaeO <antoine@quetel.pro>
2019-12-17 21:50:48 +0100

    Test:       Read & Write in vdf file (OK)

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import winreg
+from PyVDF import PyVDF
 
-
+# A bout but de changer deux valeurs dans le registre ("ActiveProcess") & ("AutoLoginUser")
 
 regActiveUser = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam\\ActiveProcess", access=winreg.KEY_ALL_ACCESS)
 
 dataActiveUser1 = 862238484
 dataActiveUser2 = 184418774
 
-winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, dataActiveUser2)
+winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, dataActiveUser1)
 print(winreg.QueryValueEx(regActiveUser, "ActiveUser"))
 
 dataStr1 = "antoineditlolotte"
 dataStr2 = "sixthaccountcsgo"
 
 regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
-winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, dataStr1)
+winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, dataStr2)
 print(winreg.QueryValueEx(regLastLogin, "AutoLoginUser"))
+
+# A pour but de changer le status dans le fichier ("loginusers.vdf") ce qui change le dernier utlisateur connectés
+
+
+vdf = PyVDF()
+vdf.load('loginusers.vdf')
+
+vdf.edit("users.76561198822504212.MostRecent", "0")
+vdf.write_file('loginusers.vdf')
+User = vdf["users"]["76561198822504212"]
+State = vdf["users"]["76561198822504212"]["MostRecent"]
+print(User)
+print(State)
