e2157bdf0b8752beaa401085b544e26b6e5e21f9
ShibaeO <antoine@quetel.pro>
2020-03-27 09:19:23 +0100

    ajouts : fontion pour recup steamid64, pseudo, ecrire
    version = 1.5.1-dev

--- ShibaeoUtlisLib.py
+++ ShibaeoUtlisLib.py
@@ -2,6 +2,28 @@ def listToString(s):
     str1 = " "
     return (str1.join(s))
 
+
+def vdfedit(mostRecentValue, steamPath):
+    from PyVDFmod import PyVDF
+    vdf = PyVDF(steamPath, mostRecentValue)
+    vdf.load("{]\\config\\loginusers.vdf".format(steamPath))
+    vdf.edit("users.76561198822504212.MostRecent", mostRecentValue)
+    vdf.write_file("{]\\config\\loginusers.vdf".format(steamPath))
+
+def vdfGrabSteamId(steamPath, account):
+    from PyVDFmod import PyVDF
+    vdf = PyVDF()
+    vdf.load("{}/config/config.vdf".format(steamPath))
+    usr = vdf["InstallConfigStore.Software.Valve.Steam.Accounts.{}.SteamID".format(account)]
+    print(usr)
+
+def vdfGrabPseudo(steamPath, steamId64):
+        from PyVDFmod import PyVDF
+        vdf = PyVDF()
+        vdf.load("{}/config/loginusers.vdf".format(steamPath))
+        pseudo = vdf["users.{}.PersonaName".format(steamId64)]
+        print(pseudo)
+
 def accountIniRemove(file, section):
     import configparser
     p = configparser.ConfigParser()
@@ -27,8 +49,17 @@ def addIniAccount(file, accountId, hexValue, steamIdValue):
         p.write(configIni)
         configIni.truncate()
 
+def setIniValue(file, section, value):
+    import configparser
+    p = configparser.ConfigParser()
+    with open(file, "r+") as configIni:
+        p.read_file(configIni)
+        p.set(section, "currentuser", value)
+        configIni.seek(0)
+        p.write(configIni)
+        configIni.truncate()
 
-def regModAutologin (AccountName):
+def regModAutologin(AccountName):
     import winreg
     regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
     winreg.SetValueEx(regLastLogin, "AutoLoginUser", 0, winreg.REG_SZ, AccountName)
@@ -36,11 +67,18 @@ def regModAutologin (AccountName):
     print("le compte acctuelement connecter est : " + account)
 
 
-def regModActiveProcess (accountValue):
+def regModActiveUser(accountValue):
     import winreg
     regActiveUser = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam\\ActiveProcess", access=winreg.KEY_ALL_ACCESS)
     winreg.SetValueEx(regActiveUser, "ActiveUser", 0, winreg.REG_DWORD, accountValue)
     decValue, idex = winreg.QueryValueEx(regActiveUser, "ActiveUser")
+    print(decValue)
+
+def regModRemPass(value):
+    import winreg
+    regActiveUser = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam", access=winreg.KEY_ALL_ACCESS)
+    winreg.SetValueEx(regActiveUser, "RememberPassword", 0, winreg.REG_DWORD, value)
+    decValue, idex = winreg.QueryValueEx(regActiveUser, "RememberPassword")
     print(decValue)
 
 
@@ -50,8 +88,36 @@ def regQuerryCurrenLogged():
         account, index = winreg.QueryValueEx(regLastLogin, "AutoLoginUser")
         return account
 
+def regQuerryCurrenUser():
+        import winreg
+        regLastLogin = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\Valve\\Steam\\ActiveProcess", access=winreg.KEY_ALL_ACCESS)
+        account, index = winreg.QueryValueEx(regLastLogin, "ActiveUser")
+        return account
+
+
+
+def checkIfProcessRunning(processName):
+    import psutil
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+
 def guiReload():
         import subprocess
         import os
         subprocess.Popen("GUI.py 1", shell=True)
         os._exit(0)
+
+def killSteam():
+    import os
+    os.system("taskkill.exe /F /IM steam.exe")
+
+def startWithAccount(SteamPath, accountId):
+    import subprocess
+    #subprocess.Popen("{}\\Steam.exe -login {} null".format(SteamPath, accountId), shell=True)
+    subprocess.Popen("{}\\Steam.exe".format(SteamPath), shell=True)
--- vdfWrite.py
+++ vdfWrite.py
@@ -1,11 +1,35 @@
-from PyVDF import PyVDF
+#from PyVDF import PyVDF
+#
+#vdf = PyVDF()
+#vdf.load("loginusers.vdf")
+#
+#vdf.edit("users.76561198822504212.MostRecent", "4")
+#vdf.write_file("loginusers.vdf")
+#User = vdf["users"]["76561198822504212"]
+#State = vdf["users"]["76561198822504212"]["MostRecent"]
+#print(User)
+#
+def vdfedit(mostRecentValue, steamPath):
+    from PyVDFmod import PyVDF
+    vdf = PyVDF(steamPath, mostRecentValue)
+    vdf.load("{]\\config\\loginusers.vdf".format(steamPath))
+    vdf.edit("users.76561198822504212.MostRecent", mostRecentValue)
+    vdf.write_file("{]\\config\\loginusers.vdf".format(steamPath))
 
-vdf = PyVDF()
-vdf.load("loginusers.vdf")
+def vdfGrabSteamId(steamPath, account):
+    from PyVDFmod import PyVDF
+    vdf = PyVDF()
+    vdf.load("{}/config/config.vdf".format(steamPath))
+    usr = vdf["InstallConfigStore.Software.Valve.Steam.Accounts.{}.SteamID".format(account)]
+    print(usr)
 
-vdf.edit("users.76561198822504212.MostRecent", "4")
-vdf.write_file("loginusers.vdf")
-User = vdf["users"]["76561198822504212"]
-State = vdf["users"]["76561198822504212"]["MostRecent"]
-print(User)
-print(State)
+def vdfGrabPseudo(steamPath, steamId64):
+        from PyVDFmod import PyVDF
+        vdf = PyVDF()
+        vdf.load("{}/config/loginusers.vdf".format(steamPath))
+        pseudo = vdf["users.{}.PersonaName".format(steamId64)]
+        print(pseudo)
+
+
+vdfGrabPseudo("j:/Steam", 76561198822504212)
+#fix vdfGrabSteamId() qui trouve pas le steam id des name avec @
